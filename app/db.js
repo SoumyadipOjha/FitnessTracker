6f130b81060f4cafae9b549482caff8316c4b7aa
Soumyadip Ojha <soumyadipojha635@gmail.com>
2024-04-09 01:46:24 +0530

    first commit

---
+++ app/db.js
@@ -0,0 +1,12 @@
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const mongoose = require('mongoose');
+
+exports.connectMongoose =()=>{
+    mongoose.set("strictQuery", false);
+    mongoose.connect('mongodb+srv://soumyadipojha635:fyk1vGyVMDdv0KJ4@cluster0.j8cpdme.mongodb.net/', { useNewUrlParser: true })
+    .then((e)=>console.log("Connected to Mongodb => Habit-Tracker"))
+    .catch((e)=>console.log("Not Connect Mongodb", e));
+}
